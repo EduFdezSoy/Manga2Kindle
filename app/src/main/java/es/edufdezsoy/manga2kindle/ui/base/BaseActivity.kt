0df2796d10181bc19716d5029de7c11871ca2cd4
Eduardo Fernandez <yo@edufdezsoy.es>
2019-08-13 21:12:29 +0200

    created base activity

---
+++ app/src/main/java/es/edufdezsoy/manga2kindle/ui/base/BaseActivity.kt
@@ -0,0 +1,8 @@
+package es.edufdezsoy.manga2kindle.ui.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+    // TODO: create a proper drawer menu (https://mikepenz.github.io/MaterialDrawer/)
+
+}
--- app/src/main/java/es/edufdezsoy/manga2kindle/ui/main/MainActivity.kt
+++ app/src/main/java/es/edufdezsoy/manga2kindle/ui/main/MainActivity.kt
@@ -3,8 +3,9 @@ package es.edufdezsoy.manga2kindle.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import es.edufdezsoy.manga2kindle.R
+import es.edufdezsoy.manga2kindle.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

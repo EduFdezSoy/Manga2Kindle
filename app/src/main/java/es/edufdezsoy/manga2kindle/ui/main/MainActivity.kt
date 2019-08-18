b08bb84e9217edcbbd67fbde42e0edde03de6d5a
Eduardo Fernandez <yo@edufdezsoy.es>
2019-08-18 14:09:56 +0200

    changed ApiService to Singleton and implemented loggin for network calls

--- app/src/main/java/es/edufdezsoy/manga2kindle/ui/main/MainActivity.kt
+++ app/src/main/java/es/edufdezsoy/manga2kindle/ui/main/MainActivity.kt
@@ -1,13 +1,10 @@
 package es.edufdezsoy.manga2kindle.ui.main
 
 import android.os.Bundle
-import es.edufdezsoy.manga2kindle.M2kApplication
 import es.edufdezsoy.manga2kindle.R
-import es.edufdezsoy.manga2kindle.network.M2kApiService
+import es.edufdezsoy.manga2kindle.network.ApiService
 import es.edufdezsoy.manga2kindle.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,23 +13,17 @@ class MainActivity : BaseActivity() {
 
         tv1.text = "currando..."
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(M2kApplication.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val apiService = retrofit.create(M2kApiService::class.java)
 
         // load the server hello message
         Thread(Runnable {
-            val call = apiService.serverHello()
+            val call = ApiService.apiService.serverHello()
             val res = call.execute().body()
             this@MainActivity.runOnUiThread { tv1.text = res!! }
         }).start()
 
         // load an author
         Thread(Runnable {
-            val call = apiService.searchAuthor("a")
+            val call = ApiService.apiService.searchAuthor("a")
             val res = call.execute().body()?.get(9)
             this@MainActivity.runOnUiThread {
                 tv2.text = res!!.name

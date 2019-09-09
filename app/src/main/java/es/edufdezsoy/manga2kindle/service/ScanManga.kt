7a1e3cf29efd935bf419460bb235a25c6dc93259
Eduardo Fernandez <yo@edufdezsoy.es>
2019-09-09 20:41:43 +0200

    this is finished but i need it in my pc (working in the service)

--- app/src/main/java/es/edufdezsoy/manga2kindle/service/ScanManga.kt
+++ app/src/main/java/es/edufdezsoy/manga2kindle/service/ScanManga.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.util.regex.Pattern
 import kotlin.coroutines.CoroutineContext
 
 class ScanManga : Service(), CoroutineScope {
@@ -41,8 +42,63 @@ class ScanManga : Service(), CoroutineScope {
                 val docFile = DocumentFile.fromTreeUri(this@ScanManga, Uri.parse(it.path))
                 if (docFile!!.canRead()) {
                     val list = getListOfFoldersNFiles(docFile)
+                    val mangas = searchForMangas(list)
+                    val series = getSeries(list, mangas)
+
+                    if (mangas.size == 1) {
+                        Log.i(
+                            TAG,
+                            "Only one manga found for this chapters (thats the right thing!)"
+                        )
+                        val manga = M2kDatabase(this@ScanManga).MangaDao().search(mangas[0])
+
+                        mangas.forEach {
+                            // this string will be something like:
+                            // [Vol.N] Ch.N [- Chapter Name]
+                            // N can be any number, 1, 2 or 3 digits
+                            // Chapter Name can have numbers take care with it
+
+                            var chapterName = ""
+                            var chapterNum: Int
+                            var chapterVol: Int
+
+                            val str = it.split(" - ")
+
+                            // chapterName
+                            if (str.size == 2) {
+                                chapterName = str.last()
+                            } else if (str.size > 2) {
+                                var first = true
+                                str.forEach {
+                                    if (!first) {
+                                        chapterName += it
+                                    } else {
+                                        first = false
+                                    }
+                                }
+                            }
+
+                            // chapterNum
+                            chapterNum =
+
+                            // chapterVol
+
+
+                            if (M2kDatabase(this@ScanManga).ChapterDao().search(manga[0].id,))
+                        }
+                    } else {
+                        Log.e(TAG, "More than one manga found for this chapters (uuh, thats bad!)")
+                    }
 
                     list.forEach {
+                        Log.d(TAG, it)
+                    }
+
+                    mangas.forEach {
+                        Log.d(TAG, it)
+                    }
+
+                    series.forEach {
                         Log.d(TAG, it)
                     }
                 } else {
@@ -69,7 +125,7 @@ class ScanManga : Service(), CoroutineScope {
             if (it.isDirectory) {
                 supTree = getListOfFoldersNFiles(it)
 
-                tree.add(it.name!! + '/')
+                tree.add(it.name!!)
                 tree.addAll(supTree)
             } else {
                 tree.add(it.name!!)
@@ -84,7 +140,45 @@ class ScanManga : Service(), CoroutineScope {
      * @return a list of mangas
      */
     private fun searchForMangas(tree: List<String>): List<String> {
+        val mangas = ArrayList<String>()
+        val mangaPageRegex = Pattern.compile("[0-9]{3}\\.(png|jpg)")
+        var canBe = false
 
-        return tree
+        tree.asReversed().forEach {
+            if (mangaPageRegex.matcher(it).matches()) {
+                canBe = true
+            } else {
+                if (canBe)
+                    mangas.add(it)
+
+                canBe = false
+            }
+        }
+
+        return mangas
+    }
+
+    /**
+     *
+     * @return a list of series based on the list of mangas
+     */
+    private fun getSeries(tree: List<String>, mangas: List<String>): List<String> {
+        val series = ArrayList<String>()
+        var thisOne = false
+
+        mangas.forEach { manga ->
+            tree.asReversed().forEach {
+                if (thisOne) {
+                    series.add(it)
+                    thisOne = false
+                }
+
+                if (it == manga) {
+                    thisOne = true
+                }
+            }
+        }
+
+        return series
     }
 }

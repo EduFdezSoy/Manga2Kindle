e24be758f85f81592857a56c16243c23b1b16d71
Eduardo Fernandez <yo@edufdezsoy.es>
2019-10-14 21:23:42 +0200

    changed a value from chapter table, added migration and schemas

--- app/src/main/java/es/edufdezsoy/manga2kindle/data/M2kDatabase.kt
+++ app/src/main/java/es/edufdezsoy/manga2kindle/data/M2kDatabase.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import es.edufdezsoy.manga2kindle.data.dao.*
 import es.edufdezsoy.manga2kindle.data.model.*
 
 @Database(
     entities = [Folder::class, Author::class, Manga::class, Language::class, Chapter::class],
-    version = 4
+    version = 5
 )
 abstract class M2kDatabase : RoomDatabase() {
     abstract fun FolderDao(): FolderDao
@@ -29,7 +31,27 @@ abstract class M2kDatabase : RoomDatabase() {
 
         private fun buildDatabase(context: Context) =
             Room.databaseBuilder(context, M2kDatabase::class.java, "manga2kindle.db")
-                .fallbackToDestructiveMigration()
+                .addMigrations(MIGRATION_4_5)
+//                .fallbackToDestructiveMigration()
                 .build()
+
+        // Previous migrations aren't available since the app was in a really early stage, so no need to
+
+        private val MIGRATION_4_5 = object : Migration(4, 5) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // add the new column
+                database.execSQL("ALTER TABLE chapter ADD status NOT NULL DEFAULT(0)")
+                database.execSQL("UPDATE chapter SET status = (SELECT CASE WHEN sended = 1 THEN 3 ELSE 0 END FROM chapter AS b WHERE chapter.identifier = b.identifier)")
+
+                // rename the table
+                database.execSQL("ALTER TABLE chapter RENAME TO chapter_old")
+                // recreate the table without the dropped column
+                database.execSQL("CREATE TABLE IF NOT EXISTS `chapter` (`identifier` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `status` INTEGER NOT NULL, `id` INTEGER, `manga_id` INTEGER NOT NULL, `lang_id` INTEGER, `volume` INTEGER, `chapter` REAL NOT NULL, `title` TEXT, `file_path` TEXT, `checksum` TEXT, `delivered` INTEGER NOT NULL, `error` INTEGER NOT NULL, `reason` TEXT, `visible` INTEGER NOT NULL)")
+
+                // migrate data from old to new table
+                database.execSQL("INSERT INTO chapter SELECT identifier, status, id, manga_id, lang_id, volume, chapter, title, file_path, checksum, delivered, error, reason, visible FROM chapter_old")
+                database.execSQL("DROP TABLE chapter_old")
+            }
+        }
     }
 }

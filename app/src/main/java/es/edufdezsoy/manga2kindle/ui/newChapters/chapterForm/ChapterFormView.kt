99ea79ca8b0cc17b6d4e00e813a63034cee6b822
Eduardo Fernandez <yo@edufdezsoy.es>
2019-10-20 23:45:43 +0200

    solved crashes when volume was empty

--- app/src/main/java/es/edufdezsoy/manga2kindle/ui/newChapters/chapterForm/ChapterFormView.kt
+++ app/src/main/java/es/edufdezsoy/manga2kindle/ui/newChapters/chapterForm/ChapterFormView.kt
@@ -102,11 +102,7 @@ class ChapterFormView(val view: View, val controller: ChapterFormContract.Contro
         try {
             chapter.volume = view.etVolume.text.toString().toInt()
         } catch (e: Exception) {
-            if (M2kApplication.debug)
-                Log.e(
-                    M2kApplication.TAG,
-                    "Yo fucker, the volume thrown an exception in the ChapterForm"
-                )
+            chapter.volume = null
         }
         // chapter.chapter = view.etChapter.text.toString().toFloat() // chapter can not be reassigned, we expect the chapter to be correct
         chapter.title = view.tietTitle.text.toString()

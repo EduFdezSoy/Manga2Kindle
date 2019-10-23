2c85b29cd93e8899ec6a93a79615c871706829c0
Eduardo Fernandez <yo@edufdezsoy.es>
2019-10-23 22:10:04 +0200

    implemented equals & hashcode

--- app/src/main/java/es/edufdezsoy/manga2kindle/data/model/viewObject/NewChapter.kt
+++ app/src/main/java/es/edufdezsoy/manga2kindle/data/model/viewObject/NewChapter.kt
@@ -10,4 +10,21 @@ class NewChapter(
 
     val author_id: Int?,
     val author: String
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other is NewChapter)
+            if (local_id == other.local_id)
+                if (manga_local_id == other.manga_local_id)
+                    if (manga_id == other.manga_id)
+                        if (author_id == other.author_id)
+                            if (chapter == other.chapter)
+                                if (manga_title == other.manga_title)
+                                    if (author == other.author)
+                                        return true
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return local_id.hashCode() + manga_local_id.hashCode() + author_id.hashCode()
+    }
+}

4239a64ff3042aa3ecd886f96aa2c309836facac
Eduardo Fernandez <yo@edufdezsoy.es>
2019-10-28 21:30:00 +0100

    solved #80

--- app/src/main/java/es/edufdezsoy/manga2kindle/ui/hiddenChapters/HiddenChaptersView.kt
+++ app/src/main/java/es/edufdezsoy/manga2kindle/ui/hiddenChapters/HiddenChaptersView.kt
@@ -17,30 +17,30 @@ class HiddenChaptersView(val view: View, val controller: HiddenChaptersContract.
     }
 
     override fun setChapters(chapters: List<HiddenChapter>) {
-        if (chapters.isNotEmpty()) {
-            if (::adapter.isInitialized) {
-                adapter.setData(chapters)
-            } else {
-                adapter = HiddenChapterAdapter(chapters)
-                adapter.setOnClickListener(View.OnClickListener {
-                    controller.openChapterDetails(
-                        adapter.chapters.get(view.rvHiddenChapters.getChildAdapterPosition(it))
-                    )
-                })
-
-                adapter.setOnLongClickListener(View.OnLongClickListener {
-                    controller.showChapter(
-                        adapter.chapters.get(view.rvHiddenChapters.getChildAdapterPosition(it))
-                    )
-
-                    return@OnLongClickListener false
-                })
-                view.rvHiddenChapters.layoutManager = LinearLayoutManager(view.context)
-                view.rvHiddenChapters.itemAnimator = DefaultItemAnimator()
-                view.rvHiddenChapters.adapter = adapter
-                view.flBackground.visibility = View.GONE
-            }
+        if (::adapter.isInitialized) {
+            adapter.setData(chapters)
         } else {
+            adapter = HiddenChapterAdapter(chapters)
+            adapter.setOnClickListener(View.OnClickListener {
+                controller.openChapterDetails(
+                    adapter.chapters.get(view.rvHiddenChapters.getChildAdapterPosition(it))
+                )
+            })
+
+            adapter.setOnLongClickListener(View.OnLongClickListener {
+                controller.showChapter(
+                    adapter.chapters.get(view.rvHiddenChapters.getChildAdapterPosition(it))
+                )
+
+                return@OnLongClickListener false
+            })
+            view.rvHiddenChapters.layoutManager = LinearLayoutManager(view.context)
+            view.rvHiddenChapters.itemAnimator = DefaultItemAnimator()
+            view.rvHiddenChapters.adapter = adapter
+            view.flBackground.visibility = View.GONE
+        }
+
+        if (chapters.isEmpty()) {
             view.flBackground.visibility = View.VISIBLE
             view.tvViewTitle.text = view.context.getString(R.string.uploaded_chapters_empty_list)
         }

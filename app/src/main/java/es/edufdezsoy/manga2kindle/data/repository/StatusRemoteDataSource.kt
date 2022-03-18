573ca468f30e9bf3151c1b5f7518916ab874dec4
Eduardo Fernandez <yo@edufdez.es>
2022-03-18 13:52:23 +0100

    rearrange how data collects

--- app/src/main/java/es/edufdezsoy/manga2kindle/data/repository/StatusRemoteDataSource.kt
+++ app/src/main/java/es/edufdezsoy/manga2kindle/data/repository/StatusRemoteDataSource.kt
@@ -5,6 +5,7 @@ import es.edufdezsoy.manga2kindle.data.model.Status
 import es.edufdezsoy.manga2kindle.network.ApiService
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 
 class StatusRemoteDataSource(
@@ -16,19 +17,21 @@ class StatusRemoteDataSource(
 
     val statuses: Flow<List<Status>> = flow {
         while (true) {
-            val chapters = chapterRepository.getStaticAllChapters()
-                .filterNot { item -> item.status == 0 }
-                .filterNot { item -> item.status >= Status.DONE }
+            chapterRepository.getAllChapters().collect {
+                val chapters = it
+                    .filterNot { item -> item.status == 0 }
+                    .filterNot { item -> item.status >= Status.DONE }
 
-            val statuses: ArrayList<Status> = arrayListOf()
-            for (ch in chapters) {
-                if (ch.remoteId != null) {
-                    statuses.add(apiService.getChapterStatus(ch.remoteId!!))
+                val statuses: ArrayList<Status> = arrayListOf()
+                for (ch in chapters) {
+                    if (ch.remoteId != null) {
+                        statuses.add(apiService.getChapterStatus(ch.remoteId!!))
+                    }
                 }
-            }
 
-            emit(statuses)
-            delay(refreshIntervalMs)
+                emit(statuses)
+                delay(refreshIntervalMs)
+            }
         }
     }
 
